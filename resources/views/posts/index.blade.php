b4abbf9303e59607d57f12c088229e6c17e02378
Bayaniadamsasaki <2201020027@universitasbumigora.ac.id>
2024-04-27 22:45:55 +0800

    Initial commit

---
+++ resources/views/posts/index.blade.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Insert Data</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.css">
+</head>
+<body style="background: lightgray">
+
+<div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <div>
+                    <h3 class="text-center my-4">Insert Data Menggunakan Laravel 10</h3>
+                    <hr>
+                </div>
+                <div class="card">
+                    <div class="card-body">
+                        <a href="{{ route('posts.create') }}" class="btn btn-md btn-success mb-3">TAMBAH DATA</a>
+                        <table class="table table-bordered">
+                            <thead>
+                              <tr>
+                                <th scope="col">GAMBAR</th>
+                                <th scope="col">NAMA PRODUK</th>
+                                <th scope="col">DESKRIPSI</th>
+                                <th scope="col">AKSI</th>
+                              </tr>
+                            </thead>
+                            <tbody>
+                              @forelse ($posts as $post)
+                                <tr>
+                                    <td class="text-center">
+                                        <img src="{{ asset('/storage/posts/'.$post->image) }}" class="rounded" style="max-width: 150px;">
+                                    </td>
+                                    <td>{{ $post->title }}</td>
+                                    <td>{!! $post->content !!}</td>
+                                    <td class="text-center">
+                                        <form onsubmit="return confirm('Apakah Anda Yakin ?');" action="{{ route('posts.destroy', $post->id) }}" method="POST">
+                                            <a href="{{ route('posts.show', $post->id) }}" class="btn btn-sm btn-dark">SHOW</a>
+                                            <a href="{{ route('posts.edit', $post->id) }}" class="btn btn-sm btn-primary">EDIT</a>
+                                            @csrf
+                                            @method('DELETE')
+                                            <button type="submit" class="btn btn-sm btn-danger">HAPUS</button>
+                                        </form>
+                                    </td>
+                                </tr>
+                              @empty
+                                  <tr>
+                                      <td colspan="4" class="text-center">
+                                          <div class="alert alert-danger">
+                                              Data Post belum Tersedia.
+                                          </div>
+                                      </td>
+                                  </tr>
+                              @endforelse
+                            </tbody>
+                          </table>  
+                          {{ $posts->links() }}
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    <script src="//cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"></script>
+
+    <!-- <script>
+        //message with toastr
+        @if(session()->has('success'))
+        
+            toastr.success('{{ session('success') }}', 'BERHASIL!'); 
+
+        @elseif(session()->has('error'))
+
+            toastr.error('{{ session('error') }}', 'GAGAL!'); 
+            
+        @endif
+    </script> -->
+
+</body>
+</html>
